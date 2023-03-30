9d749e657ce813e31f6955f656b04cfe97a13cfa
Not An Agency! <119770944+notanagency@users.noreply.github.com>
2023-03-29 19:05:41 -0500

    Update formlab.js

--- formlab.js
+++ formlab.js
@@ -1,31 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Username Value
-  const valueInput = document.querySelector('input[username]');
-  valueInput.addEventListener("keydown", function(e) {
-    if (this.value === "" && e.key !== "Backspace" && e.key !== "Delete") {
-      this.value = "@";
-      this.setSelectionRange(1, 1);
-    }
-  });
+  document.addEventListener("DOMContentLoaded", function() {
+  const formElement = document.querySelector('form[form-lab="true"]');
 
-  // Url Value
-  const urlInput = document.querySelector('input[web-url]');
-  urlInput.addEventListener("keydown", function(e) {
-    if (this.value === "" && e.key !== "Backspace" && e.key !== "Delete") {
-      this.value = "https://";
-      this.setSelectionRange(8, 8);
-    } else if (this.value.indexOf("https://") === -1 && this.selectionStart === 0) {
-      this.value = "https://" + this.value;
-      this.setSelectionRange(8, 8);
-    }
-  });
+  if (formElement) {
+    // Username Value
+		const valueInput = document.querySelector('input[username]');
+		valueInput.addEventListener("keydown", function(e) {
+			const nonIgnoredKeys = [
+		    "Backspace", "Delete", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", // allowed keys
+		    "Tab", "ShiftLeft", "ShiftRight", "ControlLeft", "ControlRight", "CapsLock", "MetaLeft", "MetaRight", "AltLeft", "AltRight" // ignored keys
+			 ];
+			 if (this.value === "" && nonIgnoredKeys.indexOf(e.code) === -1) { // ignore non-alphanumeric keys
+			   this.value = "@";
+				 this.setSelectionRange(1, 1);
+			 }
+		 });
 
-  // Convert input to lowercase
-  const inputs = document.querySelectorAll('input[web-url], input[username]');
-
-  inputs.forEach(input => {
-    input.addEventListener('input', function() {
-      this.value = this.value.toLowerCase();
+    // Url Value
+    const urlInput = document.querySelector('input[web-url]');
+    urlInput.addEventListener("keydown", function(e) {
+      if (this.value === "" && e.key !== "Backspace" && e.key !== "Delete") {
+        this.value = "https://";
+        this.setSelectionRange(8, 8);
+      }
     });
-  });
+
+    // Convert input to lowercase
+    const inputs = document.querySelectorAll('input[web-url], input[username]');
+
+    inputs.forEach(input => {
+      input.addEventListener('input', function() {
+        this.value = this.value.toLowerCase();
+      });
+    });
+  }
 });
