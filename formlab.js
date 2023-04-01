515a1be82bcd1ba0514ba9d0de7e343dd771cc98
Not An Agency! <119770944+notanagency@users.noreply.github.com>
2023-04-01 03:49:22 -0500

    Update formlab.js

--- formlab.js
+++ formlab.js
@@ -1,36 +1,64 @@
   document.addEventListener("DOMContentLoaded", function() {
-  const formElement = document.querySelector('form[form-lab="true"]');
+    const usernameInput = document.querySelector('input[data-username]');
+    const urlInput = document.querySelector('input[data-url]');
+    const emailInput = document.querySelector('input[data-email]');
 
-  if (formElement) {
-    // Username Value
-    const valueInput = document.querySelector('input[username]');
-    valueInput.addEventListener("keydown", function(e) {
-    	const nonIgnoredKeys = [
-	  "Backspace", "Delete", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", // allowed keys
- 	  "Tab", "ShiftLeft", "ShiftRight", "ControlLeft", "ControlRight", "CapsLock", "MetaLeft", "MetaRight", "AltLeft", "AltRight" // ignored keys
-      ];
-      if (this.value === "" && nonIgnoredKeys.indexOf(e.code) === -1) { // ignore non-alphanumeric keys
-	this.value = "@";
-        this.setSelectionRange(1, 1);
-      }
-    });
+    if (usernameInput) {
+      usernameInput.addEventListener("keydown", function(e) {
+        const nonIgnoredKeys = [
+          "Backspace", "Delete", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", // allowed keys
+          "Tab", "ShiftLeft", "ShiftRight", "ControlLeft", "ControlRight", "CapsLock", "MetaLeft", "MetaRight", "AltLeft", "AltRight" // ignored keys
+        ];
+        if (this.value === "" && nonIgnoredKeys.indexOf(e.code) === -1) { // ignore non-alphanumeric keys
+          this.value = "@";
+          this.setSelectionRange(1, 1);
+        }
+      });
 
-    // Url Value
-    const urlInput = document.querySelector('input[web-url]');
-    urlInput.addEventListener("keydown", function(e) {
-      if (this.value === "" && e.key !== "Backspace" && e.key !== "Delete") {
-        this.value = "https://";
-        this.setSelectionRange(8, 8);
-      }
-    });
-
-    // Convert input to lowercase
-    const inputs = document.querySelectorAll('input[web-url], input[username]');
-
-    inputs.forEach(input => {
-      input.addEventListener('input', function() {
+      usernameInput.addEventListener('input', function() {
         this.value = this.value.toLowerCase();
       });
-    });
-  }
-});
+    }
+
+    if (urlInput) {
+      urlInput.addEventListener("keydown", function(e) {
+        const nonIgnoredKeys = [
+          "Backspace", "Delete", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", // allowed keys
+          "Tab", "ShiftLeft", "ShiftRight", "ControlLeft", "ControlRight", "CapsLock", "MetaLeft", "MetaRight", "AltLeft", "AltRight" // ignored keys
+        ];
+        if (this.value === "" && nonIgnoredKeys.indexOf(e.code) === -1) { // ignore non-alphanumeric keys
+          this.value = "https://";
+          this.setSelectionRange(8, 8);
+        }
+      });
+
+      urlInput.addEventListener('input', function() {
+        this.value = this.value.toLowerCase();
+      });
+    }
+
+    if (emailInput) {
+      const blockedDomains = emailInput.getAttribute('data-email').split(',');
+
+      emailInput.addEventListener('input', function() {
+        const emailValue = this.value.trim();
+        const atIndex = emailValue.lastIndexOf('@');
+        if (atIndex > 0) {
+          const domain = emailValue.substring(atIndex + 1);
+          if (blockedDomains.some(blockedDomain => domain.startsWith(blockedDomain))) {
+            this.setCustomValidity('Email domain not allowed');
+          } else {
+            this.setCustomValidity('');
+          }
+        }
+      });
+
+      emailInput.addEventListener('blur', function() {
+        if (!this.checkValidity()) {
+          this.setCustomValidity('Invalid email address');
+        } else {
+          this.setCustomValidity('');
+        }
+      });
+    }
+  });
